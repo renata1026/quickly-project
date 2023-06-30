a74e7cf4a59ead4526f168ff26f35abe05d754a7
Renata Reinartz <rcreinartz@gmail.com>
2023-06-30 11:29:12 -0600

    fixed-mobile-nav-menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import logoDark from '../assets/logoDark.svg';
+import LogoDark from '../assets/logoDark.svg';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-const Nav = ({ loading }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    const user = localStorage.getItem('userData');
-    setUser(user);
-    console.log(user);
-  }, [user, loading]);
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -20,7 +17,7 @@ const Nav = ({ loading }) => {
         <div className="flex items-center">
           <Link href="/">
             <Image
-              src={logoDark}
+              src={LogoDark}
               alt="logo"
               width={205}
               height={75}
@@ -30,136 +27,95 @@ const Nav = ({ loading }) => {
           </Link>
         </div>
         <ul className="flex items-center hidden space-x-8 lg:flex">
-          {user && (
-            <li>
-              <Link
-                href="/profile"
-                aria-label="Login"
-                title="Login"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-              >
-                Profile
-              </Link>
-            </li>
-          )}
-          {!user && (
-            <li>
-              <Link
-                href="/login"
-                aria-label="Login"
-                title="Login"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-              >
-                Login
-              </Link>
-            </li>
-          )}
-          {!user && (
-            <li>
-              <Link
-                href="/signup"
-                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
-                aria-label="Sign up"
-                title="Sign up"
-              >
-                Sign up
-              </Link>
-            </li>
-          )}
-
-          {user && (
-            <li>
-              <Link
-                href="/login"
-                onClick={() => {
-                  localStorage.removeItem('userData');
-                  localStorage.removeItem('jwttoken');
-                  setUser(null);
-                }}
-                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
-                aria-label="Sign up"
-                title="Sign up"
-              >
-                Logout
-              </Link>
-            </li>
-          )}
+          <li>
+            <Link
+              href="/profile"
+              aria-label="Login"
+              title="Login"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
+            >
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/login"
+              aria-label="Login"
+              title="Login"
+              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
+            >
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/signup"
+              className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
+              aria-label="Sign up"
+              title="Sign up"
+            >
+              Sign up
+            </Link>
+          </li>
         </ul>
-        <div className="lg:hidden">
-          <button
-            aria-label="Open Menu"
-            title="Open Menu"
-            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
-            onClick={() => setIsMenuOpen(true)}
-          >
-            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-              />
-              <path
-                fill="currentColor"
-                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-              />
-              <path
-                fill="currentColor"
-                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-              />
-            </svg>
-          </button>
+        <div
+          onClick={handleNav}
+          className="sm:hidden cursor-pointer pl-24 text-black"
+        >
+          <AiOutlineMenu size={25} />
         </div>
       </div>
-      {isMenuOpen && (
-        <div className="mt-4 lg:hidden">
-          <nav>
-            <ul className="space-y-4">
-              <li>
-                <Link
-                  href="/profile"
-                  aria-label="Profile"
-                  title="Profile"
-                  className="block font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-                >
-                  About us
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/login"
-                  aria-label="Login"
-                  title="Login"
-                  className="block font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/signup"
-                  className="block w-full px-6 py-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:opacity-50 focus:shadow-outline focus:outline-none"
-                  aria-label="Sign up"
-                  title="Sign up"
-                >
-                  Sign up
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <button
-            aria-label="Close Menu"
-            title="Close Menu"
-            className="absolute top-0 right-0 p-2 -mt-4 -mr-4 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:opacity-50 focus:bg-deep-purple-50"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            <svg className="w-5 h-5 text-gray-600" viewBox="0 0 24 24">
-              <path
-                fill="currentColor"
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5.3 15.88l-1.42 1.42L12 13.42l-4.88 4.88-1.42-1.42L10.58 12 5.7 7.12l1.42-1.42L12 10.58l4.88-4.88 1.42 1.42L13.42 12l4.88 4.88z"
-              />
-            </svg>
-          </button>
+      <div
+        className={
+          menuOpen
+            ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500 z-10'
+            : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+        }
+      >
+        <div className="flex w-full items-center justify-end">
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
         </div>
-      )}
+        <div className="flex-col py-4">
+          <ul>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="py-4 cursor-pointer"
+            >
+              <Link href="/">Home</Link>
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="py-4 cursor-pointer"
+            >
+              <Link href="/login">Login</Link>
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="py-4 cursor-pointer"
+            >
+              <Link href="/profile">Profile</Link>
+            </li>
+            <li
+              onClick={() => setMenuOpen(false)}
+              className="py-4 cursor-pointer"
+            >
+              <Link href="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+        <Link href="/">
+          <Image
+            src={LogoDark}
+            alt="logo"
+            width={205}
+            height={75}
+            className="cursor-pointer pt-6"
+            priority
+          />
+        </Link>
+      </div>
     </div>
   );
 };
