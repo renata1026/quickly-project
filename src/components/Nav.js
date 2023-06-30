801e79d88355d2757cddef8abe1e9ca0aa493e76
Renata Reinartz <rcreinartz@gmail.com>
2023-06-30 13:42:29 -0600

    fixed -mobile-nav-logout-button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import LogoDark from '../assets/logoDark.svg';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-const Nav = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const Nav = ({ loading }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleNav = () => {
-    setMenuOpen(!menuOpen);
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const userData = localStorage.getItem('userData');
+    setUser(userData);
+  }, [loading]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('userData');
+    localStorage.removeItem('jwttoken');
+    setUser(null);
+    setIsMenuOpen(false); // Add this line to close the menu after logout
   };
 
   return (
@@ -27,82 +41,104 @@ const Nav = () => {
           </Link>
         </div>
         <ul className="flex items-center hidden space-x-8 lg:flex">
-          <li>
-            <Link
-              href="/profile"
-              aria-label="Login"
-              title="Login"
-              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-            >
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/login"
-              aria-label="Login"
-              title="Login"
-              className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
-            >
-              Login
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/signup"
-              className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
-              aria-label="Sign up"
-              title="Sign up"
-            >
-              Sign up
-            </Link>
-          </li>
+          {user && (
+            <>
+              <li>
+                <Link
+                  href="/profile"
+                  aria-label="Profile"
+                  title="Profile"
+                  className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/login"
+                  onClick={handleLogout} // Update this line
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
+                  aria-label="Logout" // Update the aria-label and title
+                  title="Logout"
+                >
+                  Logout
+                </Link>
+              </li>
+            </>
+          )}
+          {!user && (
+            <>
+              <li>
+                <Link
+                  href="/login"
+                  aria-label="Login"
+                  title="Login"
+                  className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:opacity-50"
+                >
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/signup"
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-[#ff6661] hover:opacity-80 focus:shadow-outline focus:outline-none"
+                  aria-label="Sign up"
+                  title="Sign up"
+                >
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
         <div
           onClick={handleNav}
           className="sm:hidden cursor-pointer pl-24 text-black"
         >
-          <AiOutlineMenu size={25} />
+          {isMenuOpen ? (
+            <AiOutlineClose size={25} />
+          ) : (
+            <AiOutlineMenu size={25} />
+          )}
         </div>
       </div>
       <div
         className={
-          menuOpen
+          isMenuOpen
             ? 'fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500 z-10'
             : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
         }
       >
-        <div className="flex w-full items-center justify-end">
-          <div onClick={handleNav} className="cursor-pointer">
-            <AiOutlineClose size={25} />
-          </div>
-        </div>
         <div className="flex-col py-4">
           <ul>
             <li
-              onClick={() => setMenuOpen(false)}
+              onClick={() => setIsMenuOpen(false)}
               className="py-4 cursor-pointer"
             >
               <Link href="/">Home</Link>
             </li>
             <li
-              onClick={() => setMenuOpen(false)}
+              onClick={() => setIsMenuOpen(false)}
               className="py-4 cursor-pointer"
             >
               <Link href="/login">Login</Link>
             </li>
-            <li
-              onClick={() => setMenuOpen(false)}
-              className="py-4 cursor-pointer"
-            >
-              <Link href="/profile">Profile</Link>
-            </li>
-            <li
-              onClick={() => setMenuOpen(false)}
-              className="py-4 cursor-pointer"
-            >
-              <Link href="/signup">Sign Up</Link>
-            </li>
+            {user && (
+              <li
+                onClick={() => setIsMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                <Link href="/profile">Profile</Link>
+              </li>
+            )}
+            {!user && (
+              <li
+                onClick={() => setIsMenuOpen(false)}
+                className="py-4 cursor-pointer"
+              >
+                <Link href="/signup">Sign Up</Link>
+              </li>
+            )}
           </ul>
         </div>
         <Link href="/">
